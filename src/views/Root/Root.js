5898269918795602314ca337a564da7dbdd2e181
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-05-28 17:46:50 +0200

    Create global styles

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Button from "components/atoms/Button";
+import GlobalStyle from "theme/GlobalStyle";
 
 function Root() {
 	return (
 		<>
+			<GlobalStyle />
 			<h1>Hello World:)</h1>
 			<Button>Hello</Button>
 		</>
