05256d245d8146cef9ccdfde902abe0e259e9aaa
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-05-28 23:32:32 +0200

    Create first testing story in storybook

---
+++ src/components/atoms/Button/Button.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "./Button";
+
+export default {
+	title: "DesignSystem/Atoms/Buttons",
+	component: Button,
+};
+
+export const Primary = () => <Button>Awensome button</Button>;
