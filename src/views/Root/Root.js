3811eb4175793d70c3be3cc342068a9231cee593
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-05-28 17:39:42 +0200

    Add test component

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Button from "components/atoms/Button";
 
 function Root() {
-	return <h1>Hello World :)</h1>;
+	return (
+		<>
+			<h1>Hello World:)</h1>
+			<Button>Hello</Button>
+		</>
+	);
 }
 
 export default Root;
