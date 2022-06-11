34f762ef858437fbaec02193e34219477c9f171f
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-06-11 10:59:23 +0200

    Create new component with first styles and LavLink elements

---
+++ src/components/atoms/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+`;
+
+const Belt = styled.nav`
+	max-width: 60rem;
+	background-color: ${({ theme }) => theme.surface};
+	font-size: ${({ theme }) => theme.fontSize.xs};
+	padding: 0 5px;
+	border-radius: 0 0 25px 25px;
+`;
+
+const List = styled.ul`
+	display: flex;
+	flex-direction: row;
+	list-style-type: none;
+	padding: 0 10px;
+	margin: 0;
+`;
+
+const ListElement = styled(NavLink)`
+	padding: 10px;
+	color: ${({ theme }) => theme.on.surface};
+	text-decoration: none;
+
+	&.active {
+		color: ${({ theme }) => theme.hover.primary};
+	}
+`;
+
+const NavigationBelt = () => (
+	<Wrapper>
+		<Belt>
+			<List>
+				<ListElement to={"/"}>Home</ListElement>
+				<ListElement to={"/hens"}>Kury</ListElement>
+				<ListElement to={"/timegoals"}>Godz. Pracy</ListElement>
+			</List>
+		</Belt>
+	</Wrapper>
+);
+
+export default NavigationBelt;
