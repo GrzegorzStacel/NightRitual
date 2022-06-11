efd8ee40cc05f35b2ef8a62a72986823dda45d0e
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-06-11 11:05:39 +0200

    Add Navbar and Routes to dummy components

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,19 +1,33 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "theme/GlobalStyle";
 import { theme } from "theme/mainTheme";
 import Button from "components/atoms/Button/Button";
+import Navbar from "components/atoms/Navbar/Navbar";
+import Hens from "./Hens";
+import TimeGoals from "./TimeGoals";
+import Home from "./Home";
 
 function Root() {
 	return (
 		<>
-			<ThemeProvider theme={theme}>
-				<>
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
 					<GlobalStyle />
+					<Navbar />
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='hens' element={<Hens />} />
+						<Route path='timegoals' element={<TimeGoals />} />
+					</Routes>
 					<h1>Hello World:)</h1>
-					<Button>Hello</Button>
-				</>
-			</ThemeProvider>
+					<Button activeColor='primary'>Primary</Button>
+					<Button activeColor='secondary' secondary>
+						Secondary
+					</Button>
+				</ThemeProvider>
+			</BrowserRouter>
 		</>
 	);
 }
