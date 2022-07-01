647e0b591e733705ebafc3bd1fe4034a41098024
Grzegorz Stącel <provo@Grzegorz.localdomain>
2022-07-01 10:30:02 +0200

    Add style template

---
+++ src/components/atoms/MainWindow/MainWindow.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Window = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	color: ${({ theme }) => theme.on.surface};
+	margin-top: 50px;
+`;
+
+const Wrapper = styled.main`
+	width: 90%;
+	padding: 20px;
+	background-color: ${({ theme }) => theme.surface};
+`;
+
+const MainWindow = ({ children }) => {
+	return (
+		<Window>
+			<Wrapper>{children}</Wrapper>
+		</Window>
+	);
+};
+
+MainWindow.propTypes = {
+	children: PropTypes.string,
+};
+
+export default MainWindow;
