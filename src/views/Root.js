abcd6c6588a5c50b7e609ed55b5e7e31976341a8
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-07-02 09:43:42 +0200

    Remove tests elements

--- src/views/Root.js
+++ src/views/Root.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "theme/GlobalStyle";
 import { theme } from "theme/mainTheme";
-import Button from "components/atoms/Button/Button";
 import Navbar from "components/atoms/Navbar/Navbar";
 import Hens from "./Hens";
 import TimeGoals from "./TimeGoals";
@@ -21,11 +20,6 @@ function Root() {
 						<Route path='hens' element={<Hens />} />
 						<Route path='timegoals' element={<TimeGoals />} />
 					</Routes>
-					<h1>Hello World:)</h1>
-					<Button activeColor='primary'>Primary</Button>
-					<Button activeColor='secondary' secondary>
-						Secondary
-					</Button>
 				</ThemeProvider>
 			</BrowserRouter>
 		</>
