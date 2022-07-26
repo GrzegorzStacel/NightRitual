e0d8602a005c6b4a45e2d0e6cd4b51e3d6466b91
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-07-26 21:47:38 +0200

    Add DisplayInputs function

--- src/views/TimeGoals.js
+++ src/views/TimeGoals.js
@@ -1,7 +1,11 @@
 import MainWindow from "components/atoms/MainWindow/MainWindow";
 import React from "react";
 import styled from "styled-components";
+import formatDate from "utils/formatTIme";
 import Desc from "components/atoms/Desc/Desc";
+import Input from "components/atoms/Input/Input";
+import { nanoid } from "nanoid";
+
 const widthColumn = "135px";
 
 const Container = styled.div`
@@ -13,6 +17,19 @@ const Container = styled.div`
 	margin: 0 0 0.4em;
 	color: ${({ theme }) => theme.on["surface"]};
 `;
+
+const InsideContainer = styled.div`
+	align-items: center;
+	background: ${({ theme }) => theme["surface"]};
+	display: grid;
+	grid-template-columns: 25% 15% 25% 35%;
+	align-content: center;
+	justify-items: stretch;
+	height: 35px;
+	text-align: center;
+	width: ${widthColumn};
+`;
+
 const descriptions = ["Data", "Programowanie", "Angielski", "Sport", "Km"];
 
 const DisplayDescriptions = descriptions.map((item, index) => (
@@ -21,10 +38,63 @@ const DisplayDescriptions = descriptions.map((item, index) => (
 	</Desc>
 ));
 
+const getInputs = (index) => {
+	const row = [];
+	let id;
+
+	for (index = 0; index < 4; index++) {
+		id = nanoid();
+
+		if (index === 3) {
+			row.push(
+				<InsideContainer key={id} id={id}>
+					<Input />
+					<Desc>km</Desc>
+					<Input />
+					<Desc>m</Desc>
+				</InsideContainer>
+			);
+		} else {
+			row.push(
+				<InsideContainer key={id} id={id}>
+					<Input />
+					<Desc>h</Desc>
+					<Input />
+					<Desc>min</Desc>
+				</InsideContainer>
+			);
+		}
+	}
+
+	return row;
+};
+
+const DisplayInputs = () => {
+	const inputs = [];
+
+	for (let index = 0; index < 7; index++) {
+		inputs.push(
+			<Container key={`id-${index}`}>
+				<Input
+					readOnly
+					disabled
+					big
+					activeColor='surface'
+					defaultValue={formatDate(index)}
+				/>
+				{getInputs(4)}
+			</Container>
+		);
+	}
+
+	return inputs;
+};
+
 const TimeGoals = () => {
 	return (
 		<MainWindow>
 			<Container>{DisplayDescriptions}</Container>
+			{DisplayInputs()}
 		</MainWindow>
 	);
 };
