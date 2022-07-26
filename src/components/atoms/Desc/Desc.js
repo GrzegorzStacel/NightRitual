2632daef120d08c273f00ded6a580d47a07317ae
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-07-26 21:41:01 +0200

    Add styles

--- src/components/atoms/Desc/Desc.js
+++ src/components/atoms/Desc/Desc.js
@@ -4,6 +4,10 @@ import PropTypes from "prop-types";
 const Desc = styled.p`
 	color: ${({ activeColor, theme }) => theme.on[activeColor]};
 	font-size: ${({ theme }) => theme.fontSize.xs};
+	display: inline-block;
+	width: ${({ width }) => (width ? width : "auto")};
+	padding: 0 5px;
+	text-align: center;
 `;
 
 Desc.propTypes = {
